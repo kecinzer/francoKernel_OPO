62bc881f967e9f310723e37ae717f0b4c2e0c6b4
Petr kecinzer Řezníček <kecinzer@gmail.com>
2014-08-05 16:30:43 +0200

    3.4.25
    Signed-off-by: Petr kecinzer Řezníček <kecinzer@gmail.com>

--- include/linux/freezer.h
+++ include/linux/freezer.h
@@ -84,29 +84,28 @@ static inline bool cgroup_freezing(struct task_struct *task)
 }
 #endif /* !CONFIG_CGROUP_FREEZER */
 
-/*
- * The PF_FREEZER_SKIP flag should be set by a vfork parent right before it
- * calls wait_for_completion(&vfork) and reset right after it returns from this
- * function.  Next, the parent should call try_to_freeze() to freeze itself
- * appropriately in case the child has exited before the freezing of tasks is
- * complete.  However, we don't want kernel threads to be frozen in unexpected
- * places, so we allow them to block freeze_processes() instead or to set
- * PF_NOFREEZE if needed. Fortunately, in the ____call_usermodehelper() case the
- * parent won't really block freeze_processes(), since ____call_usermodehelper()
- * (the child) does a little before exec/exit and it can't be frozen before
- * waking up the parent.
+/**
+ * freezer_do_not_count - tell freezer to ignore %current
+ *
+ * Tell freezers to ignore the current task when determining whether the
+ * target frozen state is reached.  IOW, the current task will be
+ * considered frozen enough by freezers.
+ *
+ * The caller shouldn't do anything which isn't allowed for a frozen task
+ * until freezer_cont() is called.  Usually, freezer[_do_not]_count() pair
+ * wrap a scheduling operation and nothing much else.
  */
-
-
-/* Tell the freezer not to count the current task as freezable. */
 static inline void freezer_do_not_count(void)
 {
 	current->flags |= PF_FREEZER_SKIP;
 }
 
-/*
- * Tell the freezer to count the current task as freezable again and try to
- * freeze it.
+/**
+ * freezer_count - tell freezer to stop ignoring %current
+ *
+ * Undo freezer_do_not_count().  It tells freezers that %current should be
+ * considered again and tries to freeze if freezing condition is already in
+ * effect.
  */
 static inline void freezer_count(void)
 {
@@ -118,6 +117,11 @@ static inline void freezer_count(void)
 static inline void freezer_count_unsafe(void)
 {
 	current->flags &= ~PF_FREEZER_SKIP;
+    /*
+     * If freezing is in progress, the following paired with smp_mb()
+     * in freezer_should_skip() ensures that either we see %true
+     * freezing() or freezer_should_skip() sees !PF_FREEZER_SKIP.
+     */
 	smp_mb();
 	try_to_freeze_unsafe();
 }
@@ -132,9 +136,17 @@ static inline void freezer_count_unsafe(void)
  * state is reached.  IOW, if this function returns %true, @p is considered
  * frozen enough.
  */
-static inline int freezer_should_skip(struct task_struct *p)
+static inline bool freezer_should_skip(struct task_struct *p)
 {
-	return !!(p->flags & PF_FREEZER_SKIP);
+   /*
+    * The following smp_mb() paired with the one in freezer_count()
+    * ensures that either freezer_count() sees %true freezing() or we
+    * see cleared %PF_FREEZER_SKIP and return %false.  This makes it
+    * impossible for a task to slip frozen state testing after
+    * clearing %PF_FREEZER_SKIP.
+    */
+   smp_mb();
+   return p->flags & PF_FREEZER_SKIP;
 }
 
 /*
